5dbf62b123624e3b43a5006af701bbf4cc24c59f
Sho Kotani <shokotani@ShonoMacBook-puro.local>
2019-02-12 14:37:19 +0900

    2-10

--- Practice/src/practice02/PTra02_10.java
+++ Practice/src/practice02/PTra02_10.java
@@ -8,9 +8,9 @@ package practice02;
 
 public class PTra02_10 {
 	public static void main(String[] args) {
-		int moneyInHand = 1000;	// 所持金
-		int coffee = 120;	// コーヒーの値段
-		int snack = 200;	// スナック菓子
+		int moneyInHand = 1000; // 所持金
+		int coffee = 120; // コーヒーの値段
+		int snack = 200; // スナック菓子
 
 		/*
 		 * ●一本120円（税別）の缶コーヒーを2本
@@ -21,6 +21,8 @@ public class PTra02_10 {
 		 * ※ 日本円には小数点がありませんので、整数の値で出力してください(少数部分は切り捨て)
 		 * ※ プログラムは何行書いても良いです
 		 */
+		moneyInHand = moneyInHand - (coffee * 2 + snack);
+		System.out.println(moneyInHand);
 
 	}
 }
