0177ceb55af67d0db888efe77d4658552fdfa78f
Sho Kotani <shokotani@ShonoMacBook-puro.local>
2019-02-13 11:31:49 +0900

    5-4

--- Practice/src/practice05/PTra05_04.java
+++ Practice/src/practice05/PTra05_04.java
@@ -23,16 +23,39 @@ public class PTra05_04 {
 		 */
 
 		// ①
-		System.out.println("0～6の整数を入力してください");
+		int input = -1;
+		while (input < 0 || input > 6) {
+			System.out.println("0～6の整数を入力してください");
 
-		String line = scanner.nextLine();
+			String line = scanner.nextLine();
 
-		switch(line) {
+			input = Integer.parseInt(line);
+
+			switch (line) {
 			case "0":
 				System.out.println("日曜");
 				break;
 			case "1":
 				System.out.println("月曜");
+				break;
+			case "2":
+				System.out.println("火曜");
+				break;
+			case "3":
+				System.out.println("水曜");
+				break;
+			case "4":
+				System.out.println("木曜");
+				break;
+			case "5":
+				System.out.println("金曜");
+				break;
+			case "6":
+				System.out.println("土曜");
+				break;
+			}
+
 		}
+
 	}
 }
